bf595f5ce61993f9765c016cd4c8fcd2662af314
Samuel Cahyawijaya <samuel.cahyawijaya@gmail.com>
2024-01-26 13:31:59 +0800

    update SafeNLP code - add run script for Cendol & update model & tokenizer loading code

--- run.sh
+++ run.sh
@@ -1,8 +1,111 @@
-# Calculate safety score for GPT2-small on ToxiGen human annotated dataset
-python safety_score.py --data data/toxiGen.json --output results --model gpt2 --lmHead clm 
+# Sample Scripts
+# # Calculate safety score for GPT2-small on ToxiGen human annotated dataset
+# python safety_score.py --data data/toxiGen.json --output results --model gpt2 --lmHead clm 
+#
+# # Calculate safety score for BERT-base-uncased on ToxiGen human annotated dataset
+# python safety_score.py --data data/toxiGen.json --output results --model bert-base-uncased --lmHead mlm 
+#
+# # Calculate safety score for GPT2-small on implicitHate dataset
+# python safety_score.py --data data/implicitHate.json --output results --model gpt2 --lmHead clm 
 
-# Calculate safety score for BERT-base-uncased on ToxiGen human annotated dataset
-python safety_score.py --data data/toxiGen.json --output results --model bert-base-uncased --lmHead mlm 
+###
+# Implicit Hate Ind
+###
 
-# Calculate safety score for GPT2-small on implicitHate dataset
-python safety_score.py --data data/implicitHate.json --output results --model gpt2 --lmHead clm 
+# mT0
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/mt0-small
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/mt0-base
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/mt0-large
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/mt0-xl
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/mt0-xxl
+
+# BLOOMZ
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloomz-560m
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloomz-1b1
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloomz-1b7
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloomz-3b
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloomz-7b1
+
+# LLaMA2
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  meta-llama/Llama-2-7b-chat-hf
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  meta-llama/Llama-2-13b-chat-hf
+
+# Bactrian
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloom-7b1---MBZUAI/bactrian-x-bloom-7b1-lora
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  bigscience/bloom-7b1---haonan-li/bactrian-id-bloom-7b1-lora
+
+# SEALion & SEALLM
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  aisingapore/sealion7b-instruct-nc
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  SeaLLMs/SeaLLM-7B-Chat
+
+# Merak
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  Ichsan2895/Merak-7B-v4
+
+# Cendol-Instruct
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-small
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-base
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-large
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xl
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xxl-merged
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-7b
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-13b-merged
+
+# Cendol-Chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-small-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-base-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-large-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xl-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xxl-merged-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-7b-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-13b-merged-chat
+
+###
+# Toxigen Ind
+###
+
+# mT0
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/mt0-small
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/mt0-base
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/mt0-large
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/mt0-xl
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/mt0-xxl
+
+# BLOOMZ
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloomz-560m
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloomz-1b1
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloomz-1b7
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloomz-3b
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloomz-7b1
+
+# LLaMA2
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  meta-llama/Llama-2-7b-chat-hf
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  meta-llama/Llama-2-13b-chat-hf
+
+# Bactrian
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloom-7b1---MBZUAI/bactrian-x-bloom-7b1-lora
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  bigscience/bloom-7b1---haonan-li/bactrian-id-bloom-7b1-lora
+
+# SEALion & SEALLM
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  aisingapore/sealion7b-instruct-nc
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  SeaLLMs/SeaLLM-7B-Chat
+
+# Merak
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  Ichsan2895/Merak-7B-v4
+
+# Cendol-Instruct
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-small
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-base
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-large
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xl
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xxl-merged
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-7b
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/toxigen_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-7b
+
+# Cendol-Chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-small-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-base-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-large-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xl-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-mt5-xxl-merged-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-7b-chat
+CUDA_VISIBLE_DEVICES=1 python safety_score.py --data data/implicit_hate_ind.json --output results --lmHead clm  --model  indonlp/cendol-llama2-13b-merged-chat
--- utils.py
+++ utils.py
@@ -31,22 +31,34 @@ def load_tokenizer_and_model(args, from_tf=False):
     '''
     Load tokenizer and model to evaluate.
     '''
+    pretrained_model = args.model
+    pretrained_adapter = ''
+    if 'bactrian' in pretrained_model:
+        mpretrained_model pretrained_adapter = pretrained_model.split('---')
 
-    pretrained_weights = args.model
     if args.config:
         config = AutoConfig.from_pretrained(args.config)
     else:
         config = None
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_weights) 
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model, truncation_side='left', padding_side='left', trust_remote_code=True)
+
     # Load Masked Language Model Head
     if args.lmHead == 'mlm':
-        model = AutoModelForMaskedLM.from_pretrained(pretrained_weights,
-                                                     from_tf=from_tf, config=config)        
+        model = AutoModelForMaskedLM.from_pretrained(pretrained_model, from_tf=from_tf, config=config, trust_remote_code=True)
     # load Causal Language Model Head
     else:
-        model = AutoModelForCausalLM.from_pretrained(pretrained_weights,
-                                                     from_tf=from_tf, config=config)
-
+        if pretrained_adapter != "":
+            model = AutoModelForCausalLM.from_pretrained(pretrained_model, device_map="auto", load_in_8bit=True, trust_remote_code=True)
+            model = PeftModel.from_pretrained(model, pretrained_adapter, torch_dtype=torch.float16)
+        elif "bloom" in pretrained_model or "xglm" in pretrained_model or "gpt2" in pretrained_model or "sealion7b" in pretrained_model 
+            or "Merak" in pretrained_model or "SeaLLM" in pretrained_model or  "Llama" in pretrained_model:
+            model = AutoModelForCausalLM.from_pretrained(pretrained_model, device_map="auto", load_in_8bit=True, trust_remote_code=True)
+            if "sealion7b" in pretrained_model or  "Llama" in pretrained_model:
+                tokenizer.pad_token = tokenizer.eos_token # Use EOS to pad label
+        else:
+            model = AutoModelForSeq2SeqLM.from_pretrained(pretrained_model, device_map="auto", load_in_8bit=True, trust_remote_code=True)
+            tokenizer.pad_token = tokenizer.eos_token # Use EOS to pad label
+    
     model = model.eval()
     if torch.cuda.is_available():
         model.to('cuda')
